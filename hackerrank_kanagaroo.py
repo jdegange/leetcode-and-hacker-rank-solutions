a5eb6d5b193f56e0de7286a0e9daf82a00161e5c
jdegange <31171885+jdegange@users.noreply.github.com>
2019-12-29 11:26:55 -0500

    Create hackerrank_kanagaroo.py

---
+++ hackerrank_kanagaroo.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the kangaroo function below.
+def kangaroo(x1, v1, x2, v2):
+    x1_new = x1
+    x2_new = x2
+    for i in range(10000):
+        x1_new += v1
+        x2_new += v2
+        if x1_new == x2_new:
+            return "YES"
+            
+    return "NO"
+
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    x1V1X2V2 = input().split()
+
+    x1 = int(x1V1X2V2[0])
+
+    v1 = int(x1V1X2V2[1])
+
+    x2 = int(x1V1X2V2[2])
+
+    v2 = int(x1V1X2V2[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
