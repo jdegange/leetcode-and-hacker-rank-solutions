37eeaa400f55fc975cb2134d1a824b7acf644479
jdegange <31171885+jdegange@users.noreply.github.com>
2020-01-05 14:51:53 -0500

    Create hackerrank_countingvalleys.py

---
+++ hackerrank_countingvalleys.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingValleys function below.
+def countingValleys(n, s):
+    valleys=0
+    level = 0
+    for i in range(len(s)):
+        if s[i] == "U":
+            level +=1
+            if level == 0:
+                valleys += 1
+        else:
+            level -= 1
+    return valleys
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    s = input()
+
+    result = countingValleys(n, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
