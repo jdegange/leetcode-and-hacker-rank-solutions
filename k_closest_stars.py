23ecf4ed408743e0b24411b815ff93ab47720c63
jdegange <jdegange85@gmail.com>
2020-03-19 18:18:44 -0400

    nearest_star_wip

--- k_closest_stars.py
+++ k_closest_stars.py
@@ -1,4 +1,5 @@
-import math 
+import math
+import heapq
 
 class Star:
     def __init__(self, x: float, y: float, z: float) -> None:
@@ -8,3 +9,10 @@ class Star:
         return math.sqrt(self.x**2 +self.y**2 + self.z**2)
     def __lt__(self, rhs: 'Star') -> bool:
         return self.distance < rhs.distance
+
+    def find_closest_k_stars(stars: Iterator[star],k: int) -> List[Star]:
+        max_heap: List[Tuple[float, Star]] = []
+        for star in stars:
+            heapq.heappush(max_heap, (-star.distance, star))
+            if len(max_heap) == k+1:
+                heapq.heappop(max_heap)
