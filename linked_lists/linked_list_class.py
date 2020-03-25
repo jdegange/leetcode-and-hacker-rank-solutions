d6c77d15c107df2767bdf84fc2106b54496320e6
jdegange <jdegange85@gmail.com>
2020-03-25 09:33:12 -0400

    linked lists

---
+++ linked_lists/linked_list_class.py
@@ -0,0 +1,18 @@
+#jdegange 3/25/2020
+
+
+class LinkedList:
+    def __init__(self,List, **kwargs)
+       '''initializes linked list class'''
+       
+
+    def search_list(self, L, key):
+        while L and L.data != key:
+            L = L.next
+            #if no key L is null
+        return L
+
+    def insert_after(self, L new_node):
+        new_node.next = node.next
+        node.next = new_node:
+
